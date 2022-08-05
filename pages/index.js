56da8fddc7d03b7f661879d00fad9df4f09c278e
BBrandude <92403153+BBrandude@users.noreply.github.com>
2022-08-05 02:08:22 -0400

    antibot src

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,14 @@ import Script from 'next/script'
 export default function Home() {
   return (
     <>
-      <Script src="https://avantsecure.net/script/IU16LJh7Ofp8z02kWtdQzAMzzngdQx2DfjLOWMAygC0CJGHv0v23c2JTqkxwg7fq/avant2" strategy="beforeInteractive" />
+       <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+          !async function(){var t=function(t){var n=t+"=";let e=decodeURIComponent(document.cookie);var a=e.split(";");for(let e=0;e<a.length;e++){let t=a[e];for(;" "==t.charAt(0);)t=t.substring(1);if(0==t.indexOf(n))return t.substring(n.length,t.length)}return""}("avant"),e=new Headers;e.append("Trace",t);let n=await fetch("https://avantsecure.net/script/IU16LJh7Ofp8z02kWtdQzAMzzngdQx2DfjLOWMAygC0CJGHv0v23c2JTqkxwg7fq/avant2",{method:"GET",headers:e,redirect:"follow",credentials:"include"});e=await n.text();let a=document.createElement("script");a.type="text/javascript",a.text=e,document.body.appendChild(a)}();
+        `,
+        }}
+      />
+
       <Script type='text/javascript' src='/collect.js' strategy="beforeInteractive" />
       <div className='bg-black min-h-screen flex items-center justify-center flex-col'>
         <div className='flex flex-col items-center md:items-start md:flex-row md:space-x-10 lg:space-x-20'>
